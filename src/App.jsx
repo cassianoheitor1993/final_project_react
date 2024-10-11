8677172bd8f33d4a14de4177be055463b4ee04b4
Cassiano Medeiros <cassiano.medeiros.93@gmail.com>
2024-10-11 12:34:04 -0400

    Changes in the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import cassiPicture from './assets/cassi.jpg'
 import './App.css'
 
 function App() {
@@ -9,11 +10,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://www.linkedin.com/in/cassiano-medeiros-8b17ab46/" target="_blank">
+          <img src={cassiPicture} className="cassiano picture" alt="Cassiano Picture" />
         </a>
       </div>
       <h1>Cassiano Portfolio</h1>
